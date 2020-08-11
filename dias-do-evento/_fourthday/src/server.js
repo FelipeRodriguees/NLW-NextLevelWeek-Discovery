b190a7b0e6927dfa44e67e3dc8e4fffea33c7b59
Felipe Rodrigues <contactfeliper@gmail.com>
2020-08-11 16:59:07 -0300

    Atualização na pasta do quarto dia

--- dias-do-evento/_fourthday/src/server.js
+++ dias-do-evento/_fourthday/src/server.js
@@ -1,9 +1,9 @@
 // Dados
 const proffys = [
-    { 
+    {
         name: "Diego Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "4498498498494", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "88000000", 
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
         subject: "Química", 
         cost: "20", 
@@ -11,29 +11,17 @@ const proffys = [
         time_from: [720], 
         time_to: [1220]
     },
-    { 
-        name: "Daniele Evangelista", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-        whatsapp: "4498498498494", 
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-        subject: "Química", 
-        cost: "20", 
-        weekday: [1], 
-        time_from: [720], 
-        time_to: [1220] 
-    },
-    { 
+    {
         name: "Mayk Brito", 
-        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4", 
-        whatsapp: "4498498498494", 
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "88800000000", 
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
         subject: "Química", 
         cost: "20", 
         weekday: [1], 
         time_from: [720], 
-        time_to: [1220] 
+        time_to: [1220]
     }
-
 ]
 
 const subjects = [
@@ -50,7 +38,7 @@ const subjects = [
 ]
 
 const weekdays = [
-   
+    "Domingo",
     "Segunda-feira",
     "Terça-feira",
     "Quarta-feira",
@@ -60,56 +48,49 @@ const weekdays = [
 ]
 
 // Funcionalidades
-
-function getSubject(subjectNumber){
+function getSubject(subjectNumber) {
     const position = subjectNumber - 1
-    return subjects[position] 
+    return subjects[position]
 }
 
 function pageLanding(req, res) {
     return res.render("index.html")
 }
-
 function pageStudy(req, res) {
     const filters = req.query
     return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
-
 function pageGiveClasses(req, res) {
     const data = req.query
-
+    // Se tiver data, adicionar
     const isNotEmpty = Object.keys(data).length > 0
-    // adicionar dados a lista de proffys
-    if(isNotEmpty){
-
+    if (isNotEmpty) {
         data.subject = getSubject(data.subject)
-
+        // Adicionar data a lista de proffys
         proffys.push(data)
         return res.redirect("/study")
-    }
-    
-    return res.render("give-classes.html", {subjects, weekdays})
-    // se não, não adicionar
+    }  
+    // Se não, não mostrar a página
+    return res.render("give-classes.html", { subjects, weekdays })
 }
-
 // Servidor
 const express = require('express')
 const server = express()
 
-// Configurar nunjucks (Template Engine)
-const nunjuscks = require('nunjucks')
-nunjuscks.configure('src/views',{
+// Configuração do nunjucks (Template Angine)
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
     express: server,
     noCache: true,
 })
-
-// Inicio e configuração do servidor
+// Início e configuração do servidor
 server
-// Configurar arquivos estáticos (css, scripts, imagens)
+// Configuração dos arquivos estáticos (css, scripts, images)
 .use(express.static("public"))
+
 // Rotas da aplicação
 .get("/", pageLanding)
-.get("/study", pageStudy) 
-.get("/give-classes", pageGiveClasses)
+.get("/study", pageStudy)
+.get("/give-classes",pageGiveClasses) 
 // Start do servidor
-.listen( 5500)
+.listen(5500)
